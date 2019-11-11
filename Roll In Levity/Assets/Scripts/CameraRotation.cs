7985b92ee060cc17cf24a4e1b4fe872be6273d4f
nihalshahria <53012643+nihalshahria@users.noreply.github.com>
2019-11-11 09:10:09 +0600

    Player Movement trial

---
+++ Roll In Levity/Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, 200) * 2 * Time.deltaTime);
+    }
+}
--- Roll In Levity/Assets/Scripts/Player.cs
+++ Roll In Levity/Assets/Scripts/Player.cs
@@ -2,24 +2,25 @@
 
 public class Player : MonoBehaviour
 {
-
+    public float rotationVelocity;
     public PipeSystem pipeSystem;
     public float velocity;
     private Pipe currentPipe;
     private float distanceTraveled;
     private float deltaToRotation;
     private float systemRotation;
-    private Transform world;
-    private float worldRotation;
+    private Transform world, rotater;
+    private float worldRotation, avatarRotation;
+    //private Rigidbody rigidbody;
 
     private void Start()
     {
         world = pipeSystem.transform.parent;
+        rotater = transform.GetChild(0);
         currentPipe = pipeSystem.SetupFirstPipe();
         SetupCurrentPipe();
+        //rigidbody = GetComponent<Rigidbody>();
     }
-
-
 
     private void Update()
     {
@@ -39,8 +40,45 @@ public class Player : MonoBehaviour
             Quaternion.Euler(0f, 0f, systemRotation);
 
         transform.Rotate(new Vector3(0, 0, -200) * 2 * Time.deltaTime);
+        UpdateAvatarRotation();
     }
 
+    private void UpdateAvatarRotation()
+    {
+        avatarRotation +=
+            rotationVelocity * Time.deltaTime * Input.GetAxis("Horizontal");
+        if (avatarRotation < 0f)
+        {
+            avatarRotation += 360f;
+        }
+        else if (avatarRotation >= 360f)
+        {
+            avatarRotation -= 360f;
+        }
+        rotater.localRotation = Quaternion.Euler(avatarRotation, 0f, 0f);
+    }
+
+    /*float gMag = 9.8f;
+    float moveForce = 5f;
+    Vector3 centre = Vector3.zero;
+    private Vector2 gDir = Vector3.zero;
+    private Vector2 gPer = Vector3.zero;
+
+    void FixedUpdate()
+    {
+        gDir = transform.position == centre ? -Vector3.up : (transform.position - centre).normalized;
+        gPer = new Vector2(gDir.y, -gDir.x);
+
+        rigidbody.AddForce(gDir * gMag, ForceMode.Acceleration);
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidbody.AddForce(-gPer * moveForce);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidbody.AddForce(gPer * moveForce);
+        }
+    }*/
 
     private void SetupCurrentPipe()
     {
